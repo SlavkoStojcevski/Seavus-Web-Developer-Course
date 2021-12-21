18a54384ee5bd99e19b72c9ba1f8824a074a3d71
SlavkoStojcevski <slavko.stojcevski1990@gmail.com>
2021-12-21 17:58:49 +0100

    Initial commit

---
+++ 4 JavaScript/1 Intro/homework/script.js
@@ -0,0 +1,4 @@
+var phone_price=119.95;
+tax=0.05;
+var price=(phone_price+phone_price*tax)*30;
+alert(price);
---
+++ 4 JavaScript/1 Intro/script.js
@@ -0,0 +1,9 @@
+console.log("Hello Javascript");
+let feet=3;
+let meters=feet*0.3048;
+let width=3;
+let height=4;
+let area=width*height;
+let radius=2;
+area=radius*radius*Math.PI;
+alert(area);
