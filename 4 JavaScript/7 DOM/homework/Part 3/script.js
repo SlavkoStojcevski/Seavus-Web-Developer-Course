83258109eff36136c85d92cd3f8ae6fa75f15364
SlavkoStojcevski <slavko.stojcevski1990@gmail.com>
2022-01-11 18:15:20 +0100

    dom
    dom

--- 4 JavaScript/7 DOM/homework/Part 3/script.js
+++
@@ -1,8 +0,0 @@
-function minMaxSum(list) {
-  let min = Math.min(...list);
-  let max = Math.max(...list);
-  let sum = min + max;
-  return "Max:" + max + ", Min:" + min + ", Sum:" + sum;
-}
-arr = [1, 2, 3, 0, 5, 6, 8, 9];
-alert(minMaxSum(arr));
