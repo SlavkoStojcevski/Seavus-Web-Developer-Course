2e896477ef3873a7bfa6231ac69cbba7b225698d
SlavkoStojcevski <slavko.stojcevski1990@gmail.com>
2022-01-17 13:17:11 +0100

    Update DOM part 2
    Put elements in a list and print sum formula.

--- 4 JavaScript/7 DOM/homework/Part 2/script.js
+++ 4 JavaScript/7 DOM/homework/Part 2/script.js
@@ -1,6 +1,17 @@
 let list = [1, 2, 3, 4, 5];
 let sum = 0;
+let listed = "";
+let added = "";
+let i = 0;
 for (let n of list) {
+  listed += "<li>" + n + "</li>";
   sum += n;
+  if (i != list.length - 1) {
+    added += n + "+";
+  } else {
+    added += n + "=" + sum;
+  }
+  i++;
 }
-document.querySelector("body").innerHTML = "<li>" + list + "</li>" + sum;
+document.querySelector("body").innerHTML =
+  "<ul>" + listed + "</ul>" + sum + "<br><br>" + added;
