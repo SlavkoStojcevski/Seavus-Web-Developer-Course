1e4c64b8d3fa436102b06c3c94898ac55fbe833b
SlavkoStojcevski <slavko.stojcevski1990@gmail.com>
2022-01-17 12:40:39 +0100

    update loops
    Manually finding min and max

--- 4 JavaScript/6 Loops/homework/Part 3/script.js
+++ 4 JavaScript/6 Loops/homework/Part 3/script.js
@@ -1,6 +1,16 @@
 function minMaxSum(list) {
-  let min = Math.min(...list);
-  let max = Math.max(...list);
+  let min = list[0];
+  for (let n of list) {
+    if (n < min) {
+      min = n;
+    }
+  }
+  let max = list[0];
+  for (let n of list) {
+    if (n > max) {
+      max = n;
+    }
+  }
   let sum = min + max;
   return "Max:" + max + ", Min:" + min + ", Sum:" + sum;
 }
