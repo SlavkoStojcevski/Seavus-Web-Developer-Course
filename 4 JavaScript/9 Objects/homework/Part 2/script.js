64bded1feb3fbd49d11c74178b46aa970e8659e1
SlavkoStojcevski <slavko.stojcevski1990@gmail.com>
2022-01-19 11:10:52 +0100

    Objects homework
    Objects update

--- 4 JavaScript/9 Objects/homework/Part 2/script.js
+++ 4 JavaScript/9 Objects/homework/Part 2/script.js
@@ -1,17 +1,8 @@
-let list = [1, 2, 3, 4, 5];
-let sum = 0;
-let listed = "";
-let added = "";
-let i = 0;
-for (let n of list) {
-  listed += "<li>" + n + "</li>";
-  sum += n;
-  if (i != list.length - 1) {
-    added += n + "+";
-  } else {
-    added += n + "=" + sum;
-  }
-  i++;
-}
-document.querySelector("body").innerHTML =
-  "<ul>" + listed + "</ul>" + sum + "<br><br>" + added;
+let animal = {
+  name: prompt("Animal name: "),
+  kind: prompt("Animal kind: "),
+  speak:function(speach){
+    console.log(this.kind+" says: "+speach);
+  },
+};
+animal.speak(prompt("Animal says: "));
