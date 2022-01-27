3ee29b18ca4102b0276407c825544b650dfb86ab
SlavkoStojcevski <slavko.stojcevski1990@gmail.com>
2022-01-27 11:03:53 +0100

    Jquery homework
    format document

--- 4 JavaScript/91 JQuery/homework/Part 1/script.js
+++ 4 JavaScript/91 JQuery/homework/Part 1/script.js
@@ -1,10 +1,10 @@
-let input="<input>";
-let button="<button>print</button>";
-let h1="<h1></h1>";
+let input = "<input>";
+let button = "<button>print</button>";
+let h1 = "<h1></h1>";
 $("body").prepend(input);
 $("body").append(button);
 $("body").append(h1);
-$("button").on("click",function(){
-  $("h1").text("Hello there "+$("input").val()+"!");
-  $("h1").css("color","green");
+$("button").on("click", function () {
+  $("h1").text("Hello there " + $("input").val() + "!");
+  $("h1").css("color", "green");
 });
